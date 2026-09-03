204271a30259bc475f74b9f2c6e0f30ba15b065b
teagan-git-hub <teagan.m.tran@gmail.com>
2026-09-03 12:43:18 -0400

    Main Project Upload

---
+++ project/Scripts/main_menu.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+
+func _ready() -> void :
+	pass
+
+
+
+func _process(delta: float) -> void :
+	pass
+
+
+func _on_start_pressed() -> void :
+	get_tree().change_scene_to_file("")
+
+func _on_version_notes_pressed() -> void :
+	get_tree().change_scene_to_file("")
+
+func _on_credits_pressed() -> void :
+	get_tree().change_scene_to_file("")
