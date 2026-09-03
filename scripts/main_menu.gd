1567513b957bfbf3200bc05670813ea214f81a32
teagan-git-hub <teagan.m.tran@gmail.com>
2026-09-03 13:00:05 -0400

    Move and update main_menu script
    Relocated main_menu.gd into Scripts/ (scripts/main_menu.gd) and removed the old root-level main_menu.gd and its UID. Updated Scenes/main_menu.tscn to reference the new script path and adjusted the script UID. Updated the menu script to add onready variables, play menu music on _ready, play button click sounds on button actions, and wire navigation to the version notes and credits scenes.

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,16 +1,24 @@
 extends Node2D
 
-func _ready() -> void :
-	pass
+@onready var fade: ColorRect = $fade
+@onready var menu_music: AudioStreamPlayer2D = $"sounds/Menu Music"
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var fade_timer: Timer = $fade / fade_timer
+var button_type = null
+@onready var splash: AnimationPlayer = $Splash
+@onready var button_click: AudioStreamPlayer2D = $"sounds/button click"
 
-func _process(delta: float) -> void :
-	pass
+func _ready() -> void :
+	menu_music.play()
 
 func _on_start_pressed() -> void :
+	button_click.play()
 	get_tree().change_scene_to_file("")
 
 func _on_version_notes_pressed() -> void :
-	get_tree().change_scene_to_file("")
+	button_click.play()
+	get_tree().change_scene_to_file("res://Scenes/versionnotes.tscn")
 
 func _on_credits_pressed() -> void :
-	get_tree().change_scene_to_file("")
+	button_click.play()
+	get_tree().change_scene_to_file("res://Scenes/credits.tscn")
