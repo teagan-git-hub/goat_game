599edcaf0ffdc0a0fd63b6254f52356e7f9d4310
teagan-git-hub <teagan.m.tran@gmail.com>
2026-09-08 19:13:38 -0400

    Add sprite flipping based on movement direction
    The player's goat sprite now flips horizontally to face the direction of movement, improving visual feedback. Uses horizontal input axis to determine flip state when the character is moving.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,10 +3,15 @@ extends CharacterBody2D
 
 func _physics_process(delta: float) -> void:
 	var direction=Input.get_vector("left", "right", "up", "down")
+	var h_direction := Input.get_axis("left", "right")
 	velocity=direction*800
 	move_and_slide()
 	
 	if velocity.length()>0.0: 
 		goat_2d.play("walk_animation")
+		if h_direction < 0:
+			goat_2d.flip_h = true
+		else: 
+			goat_2d.flip_h = false
 	else:
 		goat_2d.play("idle_animation")
