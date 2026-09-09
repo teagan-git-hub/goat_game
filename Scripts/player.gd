cc1f269f8c6f20b968bc6aac0453fa3ec196f7f5
jcaprathe <322911650+jcaprathe@users.noreply.github.com>
2026-09-08 21:24:22 -0400

    feat: add battle encounter flow

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -15,3 +15,10 @@ func _physics_process(delta: float) -> void:
 			goat_2d.flip_h = false
 	else:
 		goat_2d.play("idle_animation")
+
+
+# when we collide with an enemy
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	# call the battle scene
+	get_tree().change_scene_to_file("res://Scenes/battle_screen.tscn")
+	pass # Replace with function body.
